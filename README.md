39755f3ff0c68e1064f38e9301a34e80ab35ff21
kavonrtep <petr@umbr.cas.cz>
2022-04-12 15:04:24 +0200

    files added

---
+++ README.md
@@ -0,0 +1,10 @@
+Tools copied and adapted from https://github.com/galaxyproject/galaxy
+
+These tools are not available in galaxy toolshed. This is custom repository to make tham available for installation from may own toolshed repository:
+
+
+
+
+
+
+
---
+++ copy_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/galaxyproject/galaxy/raw/dev/tools/filters/gff/gff_filter_by_attribute.py
+wget https://github.com/galaxyproject/galaxy/blob/dev/tools/filters/gff/gff_filter_by_attribute.xml
+wget https://github.com/galaxyproject/galaxy/raw/dev/tools/filters/gff/extract_GFF_Features.py
+wget https://github.com/galaxyproject/galaxy/raw/dev/tools/filters/gff/extract_GFF_Features.xml
+wget https://github.com/galaxyproject/galaxy/raw/dev/tools/filters/gff2bed.xml
+wget https://github.com/galaxyproject/galaxy/raw/dev/tools/filters/gff_to_bed_converter.py
+
